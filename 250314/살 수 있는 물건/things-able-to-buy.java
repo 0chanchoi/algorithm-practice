7d9c8169137aa2f1eeacaaa9f9d044c5d32973d5
0chanchoi <118790426+0chanchoi@users.noreply.github.com>
2025-03-14 21:57:43 +0900

    Title: 살 수 있는 물건, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250314/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if(N>1000){
+            System.out.printf("book");
+        }
+        else if(N>0){
+            System.out.printf("mask");
+        }
+        else{
+            System.out.printf("no");
+        }
+    }
+}
