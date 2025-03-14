e5dab619c771d439316074ce705b17c1cad9ad20
0chanchoi <118790426+0chanchoi@users.noreply.github.com>
2025-03-14 21:36:07 +0900

    Title: 음수 구별하기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250314/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+
+        System.out.printf("%d\n", N);
+
+        if(N<0)
+            System.out.printf("minus");
+    }
+}
