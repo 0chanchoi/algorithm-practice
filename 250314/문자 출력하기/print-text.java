6fa15d2cc9c4af8be9b2856a923bc1d0120e25c0
0chanchoi <118790426+0chanchoi@users.noreply.github.com>
2025-03-14 22:13:25 +0900

    Title: 문자 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250314/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(i=0; i<7; i++){
+            System.out.printf("%d", N);
+        }
+    }
+}
